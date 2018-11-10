e5bc6a4c6b653793da95ac03e862aef28ffcce36
Kelly D. Landers <kelly.landers@aa.com>
2018-11-10 13:38:39 -0600

    init setup and form build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'bookshelfClient';
+export class AppComponent implements OnInit {
+  newBookReviewForm: FormGroup;
+  submitted: boolean = false;
+
+  title = "Welcome to Bookshelf";
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.newBookReviewForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      author: ['', Validators.required],
+      picture: ['', Validators.required],
+      description: ['', Validators.required],
+      publishedDate: ['', Validators.required],
+      review: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,16 @@
+//Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
+//3rd party
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+//Yours
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +18,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
